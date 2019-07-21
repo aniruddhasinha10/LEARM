463f4540a630d6a72ab3d8d0e854154d7bf52efd
shashankdhar75 <shashankdhar75@gmail.com>
2019-07-21 01:33:10 -0400

    integrated audio and video capture

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 document.getElementById('id-toggle-display').addEventListener('click', function() {
-    var ele_sidebar = document.querySelector('.sidebar');
-    var ele_sidebar_expand_icon = document.querySelector('.sidebar-expand-icon');
+  var ele_sidebar = document.querySelector('.sidebar');
+  var ele_sidebar_expand_icon = document.querySelector('.sidebar-expand-icon');
 	ele_sidebar.classList.toggle('sidebarpin');
 	ele_sidebar_expand_icon.classList.toggle('show');
 });
@@ -38,3 +38,42 @@ function hideDiv(){
     preview_flag=true;
 
 }
+
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+
+    const start = () => mediaRecorder.start();
+
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    resolve({ start, stop });
+  });
+
+document.getElementById('id-trigger-audio').addEventListener('click', function() {
+    const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+    (async () => {
+	  const recorder = await recordAudio();
+	  recorder.start();
+	  await sleep(5000);
+	  const audio = await recorder.stop();
+	  audio.play();
+	})();
+});
