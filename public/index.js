595d00cc8914177302b3116497ef51e1f7c60973
shashankdhar75 <shashankdhar75@gmail.com>
2019-07-29 11:06:44 -0400

    sidebar ui divided into sections (#14)

--- public/index.js
+++ public/index.js
@@ -2,6 +2,21 @@
 var preview_flag = true;
 var stream_var;
 
+$('.ui.sidebar').sidebar({
+    context: $('.bottom.segment')
+  })
+  .sidebar('attach events', '.menu .sidebar-trigger')
+  .sidebar('setting', 'transition', 'overlay')
+  .sidebar('show');
+
+$('.menu .item')
+  .tab()
+;
+
+$('.ui.accordion')
+  .accordion()
+;
+
 document.getElementById('id-toggle-display').addEventListener('click', function() {
     var ele_sidebar = document.querySelector('.sidebar');
     var ele_sidebar_expand_icon = document.querySelector('.sidebar-expand-icon');
