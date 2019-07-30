54f83d6706fa04de3a7bb1f81c21d96021003bf4
shashankdhar75 <shashankdhar75@gmail.com>
2019-07-30 11:29:44 -0400

    Integrated preview,download in new UI. Session length indicator added #14, #18, #19

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 // Common Variables
 var preview_flag = true;
 var stream_var;
+var isSessionActive = false;
 
 $('.ui.sidebar').sidebar({
     context: $('.bottom.segment')
@@ -76,16 +77,45 @@ const recordAudio = () =>
         resolve({start, stop});
     });
 
+function updateSessionTimer(element_timer_label){
+    let start_time = new Date().getTime();
+    // Update the count down every 1 second
+    let x = setInterval(function() {
+
+      let now = new Date().getTime();
+
+      let distance = now - start_time;
+
+      // Time calculations for hours, minutes and seconds
+      let hours = ("0" + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
+      let minutes = ("0" + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
+      let seconds = ("0" + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
+
+      if(isSessionActive){
+        document.getElementById("id-label-session-length").innerHTML = hours + ":" + minutes + ":" + seconds;
+      }
+
+      if (!isSessionActive) {
+        clearInterval(x);
+      }
+    }, 1000);
+}
+
 async function recordVideo(start_record_button) {
     // start video preview
     let record_start=Date();
+    isSessionActive = true;
     await togglePreview();
     // start audio capture
     const recorder = await recordAudio();
     recorder.start();
-    const stop_record_button = document.getElementById("stop_recording");
+    const stop_record_button = document.getElementById("id-btn-stop-session");
+    const pause_record_button = document.getElementById("id-btn-pause-session");
+    const session_length_label = document.getElementById("id-label-session-length");
+
     start_record_button.setAttribute("disabled","disabled");
     stop_record_button.removeAttribute("disabled");
+    pause_record_button.removeAttribute("disabled");
 
     new Promise(async resolve => {
         const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
@@ -94,16 +124,19 @@ async function recordVideo(start_record_button) {
 
         mediaRecorder.ondataavailable = e => videoChunks.push(e.data);
         mediaRecorder.onstop = e => downloadMedia(new Blob(videoChunks),record_start,'video');
-
         mediaRecorder.start();
+        updateSessionTimer(session_length_label);
         stop_record_button.addEventListener("click", function () {            
             mediaRecorder.stop();
+            isSessionActive = false;
             // stop and download the audio
             recorder.stop(record_start);            
             stop_record_button.setAttribute("disabled","disabled");
+            pause_record_button.setAttribute("disabled","disabled");
             start_record_button.removeAttribute("disabled");
             togglePreview();
             stopStreams(stream);
+            session_length_label.innerHTML = "00:00:00";
         });
     });
 }
